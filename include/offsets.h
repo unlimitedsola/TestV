0f31f334477e9f8e79543703f65a5033038ad7ca
Sola <dev@sola.love>
2019-12-15 20:59:02 +0800

    inline pointers

--- include/offsets.h
+++ include/offsets.h
@@ -8,8 +8,8 @@ using std::vector;
 
 vector<unsigned int> OffsetsAFK = {0x2C0};
 vector<unsigned int> OffsetsWantedLevel = {0x8, 0x10B8, 0x848};
-vector<unsigned int> OffsetsGodMode = {0x8, 0x280};
-vector<unsigned int> OffsetsGodModeTwo = {0x8, 0x189};
+vector<unsigned int> OffsetsGodMode = {0x8, 0x189};
+vector<unsigned int> OffsetsHealth = {0x8, 0x280};
 vector<unsigned int> OffsetsRagdoll = {0x8, 0x10A8};
 vector<unsigned int> OffsetPlayerNav = {0x8, 0x30, 0x50};
 vector<unsigned int> OffsetPlayerPos = {0x8, 0x90};
